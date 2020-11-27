3ae36e7fd49648534d85a457c21400b366ad192c
Docfaust9 <73342909+Docfaust9@users.noreply.github.com>
2020-11-28 02:38:11 +0300

    Add files via upload

---
+++ Практика 1/CreateStruct/CreateStruct/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CreateStruct
+{
+    struct Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public Person(string _firstName, string _lastName, int _age)
+        {
+            firstName = _firstName;  
+            lastName = _lastName;
+            age = _age;
+        }
+        public override string ToString()
+        {
+            return firstName + " " + lastName + ", age " + age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Alex", "Zayc", 19); 
+            Console.WriteLine(p);
+        }
+    }
+}
